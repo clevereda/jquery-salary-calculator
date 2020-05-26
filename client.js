808e71dac133aad26a5bc565cd87ec15a36d77ae
Clever Mukori <cmukori@learnorteach.com>
2020-05-25 22:05:31 -0400

    updates

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
-
+$(document).ready(realready);
 let totalMonthly = 0;
 
 //function real ready
 function realready (){
     
 $('.submitButton').on('click', function(){
+    
     totalMonthly += ($('.annual-salary').val()/12);
     if (totalMonthly > 20000) {
         $('.amount').css('background-color', 'red');
@@ -23,32 +24,27 @@ $('.submitButton').on('click', function(){
     //Assign input values to table columns & append to DOM as a table row
     $('#tablebod').append(`<tr><td>${firstN}</td><td>${lastN}</td><td>${idnum}</td><td>${Emtitle}</td><td class="salAmount">${salaryAm}</td> <td> <button type='submit' class='btn btn-danger deletion'>Delete </button>
     </td> </tr>`);
-   
 
-
-
-    $('.deletion').on('click', function (event){
+    $('.deletion').on('click', function (event) {
         let thisTable = document.getElementById('tablebod');
-        
-       
+
+
         const element = event.target;
 
         let elementRow = element.closest('tr');
         elementRow.remove();
         totalMonthly = 0;
-        for (let i = 0; i<thisTable.rows.length; i++) {
-            totalMonthly += Number(thisTable.rows[i].cells[4].innerText); 
-            
+        for (let i = 0; i < thisTable.rows.length; i++) {
+            totalMonthly += Number(thisTable.rows[i].cells[4].innerText);
+
         }
         totalMonthly /= 12;
         elAmount.empty();
-        elAmount.append(`Total Monthly: $` + totalMonthly );
-        
-        
+        elAmount.append(`Total Monthly: $` + totalMonthly);
+
+
 
     });
-
-   
 
     //emptying input fields
     $('.first-name').val('');
@@ -57,6 +53,7 @@ $('.submitButton').on('click', function(){
     $('.Employee-title').val('');
     $('.annual-salary').val('');
 });//end of anonymous function
+
+   
 }//end of realready
 
-$(document).ready(realready);
